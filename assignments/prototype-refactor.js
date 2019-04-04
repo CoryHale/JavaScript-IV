a2f6445febd4c7e0745b51be0b546dc4067bdfde
Cory Hale <Cory@CoryHalsMacbook.sumo.io>
2019-04-03 23:22:17 -0600

    Uncommented test code for prototype-refactor.js

--- assignments/prototype-refactor.js
+++ assignments/prototype-refactor.js
@@ -112,16 +112,16 @@ const archer = new Humanoid({
     language: 'Elvish',
 });
 
-//console.log(mage.createdAt); // Today's date
-//console.log(archer.dimensions); // { length: 1, width: 2, height: 4 }
-//console.log(swordsman.healthPoints); // 15
-//console.log(mage.name); // Bruce
-//console.log(swordsman.team); // The Round Table
-//console.log(mage.weapons); // Staff of Shamalama
-//console.log(archer.language); // Elvish
-//console.log(archer.greet()); // Lilith offers a greeting in Elvish.
-//console.log(mage.takeDamage()); // Bruce took damage.
-//console.log(swordsman.destroy()); // Sir Mustachio was removed from the game.
+console.log(mage.createdAt); // Today's date
+console.log(archer.dimensions); // { length: 1, width: 2, height: 4 }
+console.log(swordsman.healthPoints); // 15
+console.log(mage.name); // Bruce
+console.log(swordsman.team); // The Round Table
+console.log(mage.weapons); // Staff of Shamalama
+console.log(archer.language); // Elvish
+console.log(archer.greet()); // Lilith offers a greeting in Elvish.
+console.log(mage.takeDamage()); // Bruce took damage.
+console.log(swordsman.destroy()); // Sir Mustachio was removed from the game.
 
 
 
@@ -219,4 +219,4 @@ function fight(player1, player2) {
 
 // FIGHT!!!
 
-// fight(hero, villain);
+fight(hero, villain);
